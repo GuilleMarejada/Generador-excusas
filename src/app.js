ea961f67bbb1ca013ee654abab7c6ba49a9427d6
Guillermo <guillermo.gil.cor@gmail.com>
2024-04-22 20:26:16 +0100

    js terminado

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./src/assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  
+  function creadorDeExcusas() {
+    const who = ["Mi perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+    const action = ["se comió", "se orinó en", "aplastó", "rompió"];
+    const what = ["mi tarea", "las llaves", "el coche"];
+    const when = [
+      "antes de la clase",
+      "justo a tiempo",
+      "cuando terminé",
+      "durante mi almuerzo",
+      "mientras programaba"
+    ];
 
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-  console.log("Hello Rigo from the console!");
-};
+    let phrase = randomWho.concat(
+      " ",
+      randomAction,
+      " ",
+      randomWhat,
+      " ",
+      randomWhen,
+      "!!"
+    );
+    
+    document.querySelector("#excusa").textContent = phrase;
+
+  }
+  creadorDeExcusas();
+}
